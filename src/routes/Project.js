cf964b90fe49891cf57929cfbbc5883d27f62ada
alex <alex@equipmoz.org>
2022-11-22 11:16:24 +0200

    pricing cards created

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../components/Footer'
 import HeroForPages from '../components/HeroForPages'
 import Navbar from '../components/Navbar'
+import PricingCard from '../components/PricingCard'
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroForPages heading='PROJECTS' text='Some of my most recent works' />
+      <PricingCard />
       <Footer />
     </div>
   )
